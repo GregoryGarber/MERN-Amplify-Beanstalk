9e96e0888865f5c9ad97e4b0a78cc94ab43a30d1
GregoryGarber <ggarber@umass.edu>
2023-01-07 18:33:06 -0500

    added proxy

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useEffect, useState } from 'react';
-import Card from './card';
-import axios from 'axios'
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Card from "./card";
+import axios from "axios";
 
-console.log(process.env.REACT_APP_URL)
+console.log(process.env.REACT_APP_URL);
 
 function App() {
-  const [content, setContent] = useState({})
+  const [content, setContent] = useState({});
 
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_URL}/getInfo`)
-    .then(res => {
-      console.log(res)
-      setContent(res.data)
-    })
-  }, [])
+    axios.get(`/getInfo`).then((res) => {
+      console.log(res);
+      setContent(res.data);
+    });
+  }, []);
 
-  console.log(content)
+  console.log(content);
 
-  const cards = Object.keys(content).length === 0 ? [] : content.result === undefined ? [] : content.result.map(obj => {
-    return <Card key={obj['_id']} name={obj["name"]} description={obj["description"]}/>
-  })
-
- 
+  const cards =
+    Object.keys(content).length === 0
+      ? []
+      : content.result === undefined
+      ? []
+      : content.result.map((obj) => {
+          return (
+            <Card
+              key={obj["_id"]}
+              name={obj["name"]}
+              description={obj["description"]}
+            />
+          );
+        });
 
   return (
     <div className="App">
-      <h1>If everything is working, this is green and there are cards below me</h1>
+      <h1>
+        If everything is working, this is green and there are cards below me
+      </h1>
       {cards}
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,50 +1,52 @@
-import express from 'express';
-import logger from 'morgan';
-import { randomBytes } from 'crypto';
-import cors from 'cors';
-import dotenv  from "dotenv";
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import express from "express";
+import logger from "morgan";
+import { randomBytes } from "crypto";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
-dotenv.config()
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 const app = express();
 let collection;
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.get('/getInfo', async (req, res) => {
-    const result = await collection.find({}).toArray();
-    console.log(result)
-    res.status(200).json({
-        result
-    })
-})
-
-
+app.get("/getInfo", async (req, res) => {
+  const result = await collection.find({}).toArray();
+  console.log(result);
+  res.status(200).json({
+    result,
+  });
+});
 
 async function connect() {
+  const client = await MongoClient.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  });
 
-    const client = await MongoClient.connect(process.env.ATLAS_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverApi: ServerApiVersion.v1,          
-    });
-
-    await init(client);
-    console.log('connected')
-  }
-
-  async function init(client) {
-    const db = client.db('Database');
-    collection = db.collection('users');
+  await init(client);
+  console.log("connected");
 }
 
+async function init(client) {
+  const db = client.db("Database");
+  collection = db.collection("users");
+}
 
+// Serve static files
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, "client/build", "build", "index.html"));
+//   });
 
 await connect();
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
-  });
+  console.log(`Listening on port ${PORT}`);
+});
